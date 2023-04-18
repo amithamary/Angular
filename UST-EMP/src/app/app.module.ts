dd5ab5ce3fbe0d36d5be7d045eec4523172cd25c
amithamary <amithamarybenny@gmail.com>
2023-04-18 11:04:23 +0530

    Added Employee Registration Form

---
+++ UST-EMP/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeRegComponent } from './employee-reg/employee-reg.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { EmployeeRegService } from './employee-reg.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeRegComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [EmployeeRegService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
